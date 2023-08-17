c5e090fdfcea1e1a017b7bac2626158bf6e02d92
SefBoonen <1boonen.sef1@gmail.com>
2023-08-17 11:46:04 +0200

    Made inheritance

--- scripts/frontiers/greedyFrontier.ts
+++ scripts/frontiers/greedyFrontier.ts
@@ -1,27 +1,9 @@
-class GreedyFrontier {
-    frontier: Nodes[];
+class GreedyFrontier extends Frontier{
     goal: number[];
 
     constructor(goal: number[]) {
-        this.frontier = [];
+        super();
         this.goal = goal;
-    }
-
-    add(node: Nodes) {
-        this.frontier.push(node);
-    }
-
-    containsState(state: number[]) {
-        for(let i = 0; i < this.frontier.length; i++) {
-            if(JSON.stringify(this.frontier[i].state) == JSON.stringify(state)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    empty() {
-        return this.frontier.length == 0;
     }
 
     remove() {
